71561af5762adcb422525c64413105523ed95c9b
sajneen <sajneensiddiqua0@gmail.com>
2022-08-23 10:31:38 +0600

    add calculation

--- js/select.js
+++ js/select.js
@@ -1,11 +1,15 @@
 const cartArray = [];
 
-function display(cartPlayer){
-//    console.log(cartPlayer); 
+function display(){
+ 
     const tableBody = document.getElementById('cart-players');
-    tableBody.innerHTML = "";
-   for(let i = 0; i < cartPlayer.length; i++){
+    tableBody.textContent = "";
+   for(let i = 0; i < cartArray.length; i++){
     // console.log(cartArray[i].Name);
+    if(i>4){
+        alert('only 5 players is allowed');
+        return;
+    }
     const name = cartArray[i].Name;
 
     const tr = document.createElement("tr");
@@ -14,20 +18,22 @@ function display(cartPlayer){
     <td> ${name}</td>
     `;
     tableBody.appendChild(tr);
+
    }
-   if(cartArray.length>=6){
-    alert('you can not buy more than 5 players')
-    return;
-   }
+   
+  
+   
 }
 
 
 function addTocart(element){
     // console.log(element.parentNode.parentNode.children)
     // console.log(element.parentNode.parentNode.children[0].innerText)
+   element.disabled = true;
     
     const playerName = element.parentNode.parentNode.children[0].innerText;
-    // 
+    
+
     
     const playerObj ={
         Name: playerName,
@@ -36,4 +42,6 @@ function addTocart(element){
 //    console.log(cartArray);
 //    console.log(cartArray.length);
 display(cartArray);
+
 }
+
